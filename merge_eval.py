2700691cf9e8058a8756896b85b9ded2ebfed7f2
akkky02 <akshatpatil7@gmail.com>
2024-02-18 01:05:02 +0000

    added fp4 experiment

--- merge_eval.py
+++ merge_eval.py
@@ -15,7 +15,7 @@ def merge_push_eval(model_name, adapter):
     # compute_dtype = getattr(torch, "float32")
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
+        bnb_4bit_quant_type="fp4",
         bnb_4bit_compute_dtype=torch.bfloat16,
         bnb_4bit_use_double_quant=False,
     )
